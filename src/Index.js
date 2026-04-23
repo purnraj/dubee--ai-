a8359fc6954c84c78f80c13cd31ed7c3f10d572c
purnraj <purvapurnaraj@gmail.com>
2026-04-23 15:41:08 +0530

    Change static files directory name to 'Public'

--- src/Index.js
+++ src/Index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 app.use("/outputs", express.static(path.join(__dirname, "outputs")));
 
 // Create folders
